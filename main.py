1ffe0cf4559b88ff287ae8e4d616545452591818
Gabriel Braga <bielbraga37@outlook.com>
2026-07-29 19:00:02 -0300

    created header

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from tkinter import *
 from tkinter import Tk, ttk
 
+# Importando pillow
+from PIL import Image, ImageTk
+
+
 colors = []
 
-#crinado uma janea vazia
+#crinado uma janela vazia
 janela = Tk()
 janela.title()
 janela.geometry('900x650') #largura e altura
@@ -22,6 +26,16 @@ frame_meio.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
 
 frame_baixo = Frame(janela, width=1043, height=00, background='#808080', relief='flat')
 frame_baixo.grid(row=2, column=0, pady=0, padx=10, sticky=NSEW)
+
+# Trabalhando no frame cima - acessando imagem
+
+app_img = Image.open('imagens/log.png')
+app_img = app_img.resize((45,45)) #reduzindo o tamanho da imagem(largura,altura)
+app_img = ImageTk.PhotoImage(app_img)
+
+app_logo = Label(frame_cima, image=app_img, text=" Orçamento pessoal", width=900, compound=LEFT, padx=5, relief='raised', anchor=NW, font=('verdana 20 bold'), background='#ffffff', fg='#000000')
+app_logo.place(x=0,y=0)
+ 
 
 
 janela.mainloop() # mostrar na tela?
